29407f0785f395a99ed6e3b9cae58abd58b2dc92
muhammad rio <muhammad.rioperm@gmail.com>
2024-06-27 15:53:31 +0700

    Update housing location component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   template: `
     <a [routerLink]="['/details', housingLocation.id]">
       <section class="listing">
